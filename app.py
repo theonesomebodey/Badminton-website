29e6f9050b6209ba7cab55fed686fa3116cf269b
theonesomebodey <42191358+theonesomebodey@users.noreply.github.com>
2025-07-09 20:59:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def volunteer():
         msg['Subject'] = "volunteer - " + fullname
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login('shawnxionggg@gmail.com', 'mwbp njni qlfo vfqf')
+        server.login('shawnxionggg@gmail.com', password)
         server.send_message(msg)
         server.quit()
         return render_template('thankyouvolunteer.html')
